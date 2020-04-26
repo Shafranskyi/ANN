cb4790e35160eb53756babad202e37c04d4778a7
OleksiyShafranskyi <shafranskyi_ak16@nuwm.edu.ua>
2020-04-26 17:23:02 +0300

    Add project files.

---
+++ ANN/Controllers/DefaultController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ANN.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DefaultController : ControllerBase
+    {
+        private readonly ILogger<DefaultController> _logger;
+
+        public DefaultController(ILogger<DefaultController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public ActionResult Get()
+        {
+
+
+            return Ok("Ok");
+        }
+    }
+}
